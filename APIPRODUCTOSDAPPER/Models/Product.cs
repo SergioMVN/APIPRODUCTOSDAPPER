3a6299ba22ba5151259e880df79756d2120d43f2
SerDev <108685183+SergioMVN@users.noreply.github.com>
2024-08-24 22:04:09 -0400

    Comentario Models Producto.cs
    se agrego comentario propiedades productos.

--- APIPRODUCTOSDAPPER/Models/Product.cs
+++ APIPRODUCTOSDAPPER/Models/Product.cs
@@ -2,8 +2,8 @@
 {
     public class Product
     {
+        //Propiedades de productos
         public int Id { get; set; }
-        
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
     }
